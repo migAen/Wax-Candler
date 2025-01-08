18ff3fe0f4f2bcec23ab6786d176cdb454b12e8e
Miguel Aenlle <migueltm198@hotmail.com>
2025-01-08 14:58:45 +0100

    Homepage design made

--- Wax-Candler/src/Components/NavBar/Navbar.jsx
+++ Wax-Candler/src/Components/NavBar/Navbar.jsx
@@ -2,17 +2,28 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary sticky-top">
       <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <a className="navbar-brand" href="/">
           <img
-            src="/logoWithoutBackground.png" 
+            src="/Logo-Row-NoB.png" 
             alt="Logo"
-            style={{ height: '60px', width: 'auto' }}
+            style={{ height: '100px'}}
           />
         </a>       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/CandleCreator">Candle Creator</a>
             </li>
@@ -25,7 +36,7 @@ function Navbar() {
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
-                aria-expanded="true"
+                aria-expanded="false"
               >
                 Calculator
               </a>
